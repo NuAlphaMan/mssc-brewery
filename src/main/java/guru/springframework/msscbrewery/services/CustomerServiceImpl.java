a76c2d850ae63124a66dad4680af99611cf4ef2f
NuAlphaMan <NuAlphaMan@users.noreply.github.com>
2022-05-15 17:26:04 -0400

    Initial commit of code for the Brewery.

---
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -0,0 +1,36 @@
+package guru.springframework.msscbrewery.services;
+
+import guru.springframework.msscbrewery.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@Slf4j
+public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public CustomerDto getCustomerById(UUID customerId) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .name("Customer One")
+                .build();
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        //Todo: add real functionality to update customer
+    }
+
+    @Override
+    public void deleteCustomerById(UUID customerId) {
+        log.info("Deleting customer with id: " + customerId.toString());
+    }
+}
